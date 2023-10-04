9db8692dd0cfeb2e472f5376ce0874c72e01589a
Augustus Souza dos Santos <augustus.mp2@gmail.com>
2023-10-04 13:48:09 -0300

    final

--- scripts/map.js
+++ scripts/map.js
@@ -1,25 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Select elements
+
     var plateInput = document.getElementById('plate');
     var brandInput = document.getElementById('brand');
     var createCarButton = document.querySelector('#container-new-car button');
     var containerInfo = document.getElementById('container-info');
 
-    // Event listener for the button click
     createCarButton.addEventListener('click', function () {
-        // Get input values
+
         var plateValue = plateInput.value;
         var brandValue = brandInput.value;
 
-        // Create a new div element
         var newCarDiv = document.createElement('div');
         newCarDiv.className = 'container-car';
         newCarDiv.textContent = 'Placa: ' + plateValue + ', Marca: ' + brandValue;
 
-        // Append the new div to container-info
         containerInfo.appendChild(newCarDiv);
 
-        // Clear input fields after creating the new car
         plateInput.value = '';
         brandInput.value = '';
     });
